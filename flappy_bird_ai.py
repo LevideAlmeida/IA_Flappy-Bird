2da751bd677ab758b71b0f4860e1e1d822bdd704
Levi <thegood878@gmail.com>
2023-12-19 19:59:28 -0300

    ia playing flappy bird

---
+++ flappy_bird_ai.py
@@ -0,0 +1,326 @@
+import pygame
+from pathlib import Path
+import random
+import neat
+
+generation = 0
+
+pygame.init()
+pygame.font.init()
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 800
+ROOT_FOLDER = Path(__file__).parent
+
+BACKGROUND_IMAGE = pygame.transform.scale2x(
+    pygame.image.load(
+        ROOT_FOLDER / "imgs" / "bg.png"
+    )
+)
+
+PIPE_IMAGE = pygame.transform.scale2x(
+    pygame.image.load(
+        ROOT_FOLDER / "imgs" / "pipe.png"
+    )
+)
+
+GROUND_IMAGE = pygame.transform.scale2x(
+    pygame.image.load(
+        ROOT_FOLDER / "imgs" / "base.png"
+    )
+)
+
+BIRD_IMAGES = [
+    pygame.transform.scale2x(
+        pygame.image.load(ROOT_FOLDER / "imgs" / "bird1.png")
+    ),
+    pygame.transform.scale2x(
+        pygame.image.load(ROOT_FOLDER / "imgs" / "bird2.png")
+    ),
+    pygame.transform.scale2x(
+        pygame.image.load(ROOT_FOLDER / "imgs" / "bird3.png")
+    ),
+    pygame.transform.scale2x(
+        pygame.image.load(ROOT_FOLDER / "imgs" / "bird2.png")
+    ),
+]
+
+pygame.display.set_caption("Flappy Bird")
+
+POINTS_FONT = pygame.font.SysFont("", 50, False, False)
+
+
+class Bird(pygame.sprite.Sprite):
+    # Variaveis de animação de rotação
+    MAX_ROTATION = 25
+    ROTANTION_SPEED = 20
+    ANIMATED_TIME = 5
+
+    def __init__(self, x, y):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.angle = 0
+        self.speed = 0
+        self.height = self.y
+        self.time = 0
+        self.image_index = 0
+        self.images = BIRD_IMAGES
+        self.image = self.images[0]
+
+    def jump(self):
+        self.speed = -10.5
+        self.time = 0
+        self.height = self.y
+
+    def move(self):
+        self.time += 1
+        # S = So + Vo*t + at²/2
+        displacement = self.speed * self.time + 1.5 * (self.time**2)
+
+        if displacement > 16:
+            displacement = 16
+        elif displacement < 0:
+            displacement -= 2
+
+        self.y += displacement
+
+        if displacement < 0 or self.y < (self.height + 50):
+            if self.angle < self.MAX_ROTATION:
+                self.angle = self.MAX_ROTATION
+        else:
+            if self.angle > -90:
+                self.angle -= self.ROTANTION_SPEED
+
+    def draw(self, screen):
+        # definir qual imagem do passaro vai usar
+        self.image_index += 1
+
+        if self.image_index < self.ANIMATED_TIME:
+            self.image = self.images[0]
+        elif self.image_index < self.ANIMATED_TIME*2:
+            self.image = self.images[1]
+        elif self.image_index < self.ANIMATED_TIME*3:
+            self.image = self.images[2]
+        elif self.image_index < self.ANIMATED_TIME*4:
+            self.image = self.images[1]
+        elif self.image_index >= self.ANIMATED_TIME*4 + 1:
+            self.image = self.images[0]
+            self.image_index = 0
+
+        # se o passaro tiver caindo eu não vou bater asa
+        if self.angle <= -80:
+            self.image = self.images[1]
+            self.image_index = self.ANIMATED_TIME*2
+
+        # desenhar a imagem
+        rotated_image = pygame.transform.rotate(self.image, self.angle)
+        image_center_position = self.image.get_rect(
+            topleft=(self.x, self.y)).center
+        rect = rotated_image.get_rect(center=image_center_position)
+        screen.blit(rotated_image, rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.image)
+
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, position_x):
+        super().__init__()
+        self.x = position_x
+        self.height = 0
+        self.pipes_distance = 200
+        self.ground_pipe_position = 0
+        self.top_pipe_position = 0
+        self.speed = 5
+        self.vertical_speed = random.choice([5, -5])
+        self.ground_pipe = PIPE_IMAGE
+        self.top_pipe = pygame.transform.flip(PIPE_IMAGE, False, True)
+        self.passed = False
+        self.define_height()
+
+    def define_height(self):
+        self.height = random.randrange(100, 450)
+        self.top_pipe_position = self.height - self.top_pipe.get_height()
+        self.ground_pipe_position = self.height + self.pipes_distance
+
+    def move(self):
+        self.x -= self.speed
+
+        if self.height > SCREEN_HEIGHT - 360:
+            self.vertical_speed = -5
+        elif self.height < 100:
+            self.vertical_speed = 5
+
+        self.height += self.vertical_speed
+        self.top_pipe_position = self.height - self.top_pipe.get_height()
+        self.ground_pipe_position = self.height + self.pipes_distance
+
+    def draw(self, screen):
+        screen.blit(self.top_pipe, (self.x, self.top_pipe_position))
+        screen.blit(self.ground_pipe, (self.x, self.ground_pipe_position))
+
+    def collided(self, bird):
+        bird_mask = bird.get_mask()
+        top_pipe_mask = pygame.mask.from_surface(self.top_pipe)
+        ground_pipe_mask = pygame.mask.from_surface(self.ground_pipe)
+
+        top_distance = (self.x - bird.x,
+                        self.top_pipe_position - round(bird.y))
+        ground_distance = (self.x - bird.x,
+                           self.ground_pipe_position - round(bird.y))
+
+        top_point = bird_mask.overlap(top_pipe_mask, top_distance)
+        ground_point = bird_mask.overlap(ground_pipe_mask, ground_distance)
+
+        if top_point or ground_point:
+            return True
+        else:
+            return False
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, y):
+        super().__init__()
+        self.image = GROUND_IMAGE
+        self.width = self.image.get_width()
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.width
+        self.speed = 5
+
+    def move(self):
+        self.x1 -= self.speed
+        self.x2 -= self.speed
+
+        if self.x1 + self.width - 5 < 0:
+            self.x1 = self.width
+        if self.x2 + self.width - 5 < 0:
+            self.x2 = self.width
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x1, self.y))
+        screen.blit(self.image, (self.x2, self.y))
+
+
+def draw_screen(screen, birds, pipes, ground, score):
+    screen.blit(BACKGROUND_IMAGE, (0, -200))
+    for bird in birds:
+        bird.draw(screen)
+
+    for pipe in pipes:
+        pipe.draw(screen)
+
+    text = POINTS_FONT.render(f"Score: {score}", True, (0, 0, 0))
+    screen.blit(text, (10, text.get_height()))
+
+    text = POINTS_FONT.render(f"Generation: {generation}", True, (0, 0, 0))
+    screen.blit(text, (SCREEN_WIDTH - text.get_width(), text.get_height()))
+
+    ground.draw(screen)
+    pygame.display.flip()
+
+
+def main(genomes, config):
+    global generation
+    generation += 1
+
+    neural_networks = []
+    genomes_list = []
+    birds = []
+
+    for _, genome in genomes:
+        network = neat.nn.FeedForwardNetwork.create(genome, config)
+        neural_networks.append(network)
+        genome.fitness = 0
+        genomes_list.append(genome)
+        birds.append(Bird(200, 300))
+
+    ground = Ground(SCREEN_HEIGHT - 160)
+    pipes = [Pipe(SCREEN_WIDTH)]
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    score = 0
+    clock = pygame.time.Clock()
+    fps = 30
+
+    while True:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # noqa
+                pygame.quit()
+                exit()
+
+        pipe_index = 0
+        if len(birds) > 0 and len(pipes) > 0:
+            if birds[0].x > (pipes[0].x + pipes[0].top_pipe.get_width()):
+                pipe_index = 1
+        else:
+            break
+
+        for i, bird in enumerate(birds):
+            bird.move()
+            genomes_list[i].fitness += 0.1
+            inputs = (bird.x,
+                      abs(bird.y - pipes[pipe_index].height),
+                      abs(bird.y - pipes[pipe_index].ground_pipe_position),
+                      )
+            outputs = neural_networks[i].activate(inputs)
+            if outputs[0] > 0.5:
+                bird.jump()
+
+        ground.move()
+
+        add_pipe = False
+        remove_pipes = []
+        for pipe in pipes:
+            for i, bird in enumerate(birds):
+                if pipe.collided(bird):
+                    birds.pop(i)
+                    genomes_list[i].fitness -= 1
+                    genomes_list.pop(i)
+                    neural_networks.pop(i)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+
+            pipe.move()
+
+            if pipe.x < -(pipe.ground_pipe.get_width()):
+                remove_pipes.append(pipe)
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(SCREEN_WIDTH))
+            for genome in genomes_list:
+                genome.fitness += 5
+
+        for pipe in remove_pipes:
+            pipes.remove(pipe)
+
+        for i, bird in enumerate(birds):
+            if (bird.y + bird.image.get_height()) >= ground.y or bird.y <= 0:
+                birds.pop(i)
+                genomes_list[i].fitness -= 10
+                genomes_list.pop(i)
+                neural_networks.pop(i)
+
+        draw_screen(screen, birds, pipes, ground, score)
+
+
+def run(config_path):
+    config = neat.Config(neat.DefaultGenome,
+                         neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet,
+                         neat.DefaultStagnation,
+                         config_path)
+    population = neat.Population(config)
+    population.add_reporter(neat.StdOutReporter(True))
+    population.add_reporter(neat.StatisticsReporter())
+
+    population.run(main, 50)
+
+
+if __name__ == "__main__":
+    CONFIG_PATH = ROOT_FOLDER / "config.txt"
+    run(CONFIG_PATH)
